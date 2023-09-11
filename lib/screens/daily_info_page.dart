cb9b38de0f0398ef1eb8daaf493ee74fd5474cfb
egebakisoglu <egebakisoglu@gmail.com>
2023-09-11 15:03:34 +0300

    added daily info (max temp, min temp, average weather all day)

--- lib/screens/daily_info_page.dart
+++ lib/screens/daily_info_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class DailyInfoPage extends StatefulWidget {
+  final int pageIndex;
   final List hourlyWeather;
   final List hourlyTemp2m;
   final String date;
@@ -10,6 +11,7 @@ class DailyInfoPage extends StatefulWidget {
 
   const DailyInfoPage({
     super.key,
+    required this.pageIndex,
     required this.hourlyWeather,
     required this.hourlyTemp2m,
     required this.date,
@@ -32,6 +34,79 @@ class _DailyInfoPageState extends State<DailyInfoPage> {
         backgroundColor: Colors.transparent,
         foregroundColor: Colors.black,
         title: Text(widget.date),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            margin: const EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.white,
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Image.asset("assets/weather/${widget.weatherCode}.png",
+                width: 80,
+                height: 80,
+                fit: BoxFit.fill,
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 50,
+                  child: VerticalDivider(
+                    color: Colors.grey[300],
+                    thickness: 3,
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          color: Colors.orange[800],
+                          fontSize: 25,
+                        ),
+                        children: [
+                          const TextSpan(text: "Max: "),
+                          TextSpan(text: "${widget.maxTemp2m} °C", style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          color: Colors.blue[800],
+                          fontSize: 25,
+                        ),
+                        children: [
+                          const TextSpan(text: "Min: "),
+                          TextSpan(text: "${widget.minTemp2m} °C", style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+
+        ],
       ),
     );
   }
