1cac78d1d95082822d25832e270556f611e2b9cf
egebakisoglu <egebakisoglu@gmail.com>
2023-09-11 20:08:18 +0300

    added dispose

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -18,6 +18,12 @@ class _HomePageState extends State<HomePage> {
   final GlobalController globalController = Get.put(GlobalController(), permanent: true);
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
