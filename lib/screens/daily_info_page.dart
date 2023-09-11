5e0c4f3a52784e7820cc58af42396fc4835be289
egebakisoglu <egebakisoglu@gmail.com>
2023-09-11 15:50:08 +0300

    added hourly weather inside all days

--- lib/screens/daily_info_page.dart
+++ lib/screens/daily_info_page.dart
@@ -25,6 +25,13 @@ class DailyInfoPage extends StatefulWidget {
 }
 
 class _DailyInfoPageState extends State<DailyInfoPage> {
+  int listStartIndex = 0;
+
+  @override
+  void initState() {
+    listStartIndex = widget.pageIndex * 24;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,79 +42,135 @@ class _DailyInfoPageState extends State<DailyInfoPage> {
         foregroundColor: Colors.black,
         title: Text(widget.date),
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Container(
-            padding: const EdgeInsets.all(20),
-            margin: const EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: Colors.white,
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.grey,
-                  spreadRadius: 1,
-                  blurRadius: 5,
-                  offset: Offset(0, 4),
-                ),
-              ],
-            ),
-            child: Row(
-              children: [
-                Image.asset("assets/weather/${widget.weatherCode}.png",
-                width: 80,
-                height: 80,
-                fit: BoxFit.fill,
-                ),
-                SizedBox(
-                  height: 100,
-                  width: 50,
-                  child: VerticalDivider(
-                    color: Colors.grey[300],
-                    thickness: 3,
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    RichText(
-                      text: TextSpan(
-                        style: TextStyle(
-                          color: Colors.orange[800],
-                          fontSize: 25,
-                        ),
-                        children: [
-                          const TextSpan(text: "Max: "),
-                          TextSpan(text: "${widget.maxTemp2m} °C", style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    RichText(
-                      text: TextSpan(
-                        style: TextStyle(
-                          color: Colors.blue[800],
-                          fontSize: 25,
-                        ),
-                        children: [
-                          const TextSpan(text: "Min: "),
-                          TextSpan(text: "${widget.minTemp2m} °C", style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-
+          headlineContainer(),
+          hourlyValues(),
+          SizedBox(height: 40),
         ],
       ),
     );
+  }
+
+  GridView hourlyValues() {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+      ),
+      scrollDirection: Axis.vertical,
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: 24,
+      itemBuilder: (context, index){
+        return Container(
+          padding: EdgeInsets.all(5),
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          width: 100,
+          decoration: BoxDecoration(
+            color: Colors.lightBlue[100],
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 3,
+                spreadRadius: 1,
+                color: Colors.grey,
+                offset: Offset(4,0),
+              ),
+            ]
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                index < 10
+                    ? "0$index:00"
+                    : "$index:00",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              Image.asset("assets/weather/${widget.hourlyWeather[listStartIndex + index]}.png"),
+              Text(
+                "${widget.hourlyTemp2m[listStartIndex + index]} °C",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+
+  Container headlineContainer() {
+    return Container(
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.all(30),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.grey,
+                spreadRadius: 1,
+                blurRadius: 5,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              Image.asset("assets/weather/${widget.weatherCode}.png",
+              width: 80,
+              height: 80,
+              fit: BoxFit.fill,
+              ),
+              SizedBox(
+                height: 100,
+                width: 50,
+                child: VerticalDivider(
+                  color: Colors.grey[300],
+                  thickness: 3,
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        color: Colors.orange[800],
+                        fontSize: 25,
+                      ),
+                      children: [
+                        const TextSpan(text: "Max: "),
+                        TextSpan(text: "${widget.maxTemp2m} °C", style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        color: Colors.blue[800],
+                        fontSize: 25,
+                      ),
+                      children: [
+                        const TextSpan(text: "Min: "),
+                        TextSpan(text: "${widget.minTemp2m} °C", style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
   }
 }
